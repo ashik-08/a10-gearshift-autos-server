76b78a7d3b431e808cffde4de0a36ed57406bcbd
ashik-08 <ashikurrahman.8october@gmail.com>
2023-10-19 09:09:29 +0600

    query to check that if there is already a car with that name

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -9,7 +10,6 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ya8cack.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,17 +44,37 @@ async function run() {
       const newCar = req.body;
       console.log(brandName, newCar);
 
+      // find the collection based on brandName
       const collections = await database.listCollections().toArray();
       const collectionName = collections.find(
         (collection) => collection.name === brandName
       );
+      // collection found
       if (collectionName) {
         const brandNameCollection = collectionName.name;
-        const result = await database
+        // query to find all data in the collection
+        const query = await database
           .collection(brandNameCollection)
-          .insertOne(newCar);
-        res.send(result);
-      } else {
+          .find()
+          .toArray();
+        // check if there is already an item with that carName
+        const found = query.find(
+          (search) => search.name === newCar.name && search.type === newCar.type
+        );
+        console.log(found.name);
+        if (found) {
+          res.send("Already exists in DB");
+        }
+        // insert a new car into the collection
+        else {
+          const result = await database
+            .collection(brandNameCollection)
+            .insertOne(newCar);
+          res.send(result);
+        }
+      }
+      // collection not found
+      else {
         res.send(`No such brand ${brandName}`);
       }
     });
